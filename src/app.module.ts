2374111fb776d0968471b5701ed351ece087505c
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2024-01-24 07:22:23 +1100

    add authmiddleware to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UserModule } from './app/user/user.module';
 import { AuthModule } from './app/auth/auth.module';
 import { RedisModule } from './redis/redis.module';
+import { AuthMiddleware } from './middleware/auth.middleware';
 
 @Module({
   imports: [UserModule, AuthModule, RedisModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(AuthMiddleware).forRoutes('*');
+  }
+}
