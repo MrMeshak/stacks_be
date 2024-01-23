5ad23cfff21ee78065dca57a323b3a65dd2f2736
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2024-01-24 03:41:49 +1100

    adds reddis module, redis service and resdis provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './app/user/user.module';
 import { AuthModule } from './app/auth/auth.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, RedisModule],
   controllers: [],
   providers: [],
 })
