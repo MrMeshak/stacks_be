f8eb97b6447da4a0f751255c86fca7b14cd9aa52
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2024-03-03 14:02:29 +1100

    adds createTask enpoint to taskModule

--- src/app/task/task.controller.ts
+++ src/app/task/task.controller.ts
@@ -1,14 +1,20 @@
 import {
+  Body,
   Controller,
   Get,
   NotFoundException,
   Param,
   ParseUUIDPipe,
+  Post,
+  UseGuards,
 } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { AuthContextDec } from 'src/utils/decorators/auth-context.decorator';
 import { AuthContext } from 'src/middleware/auth.middleware';
+import { CreateTaskDto } from './dto/createTask.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
 
+@UseGuards(AuthGuard)
 @Controller('tasks')
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
@@ -28,5 +34,18 @@ export class TaskController {
     }
 
     return task;
+  }
+
+  @Post(':stackId')
+  async createTask(
+    @Param('stackId', ParseUUIDPipe) stackId: string,
+    @Body() data: CreateTaskDto,
+    @AuthContextDec() authContext: AuthContext,
+  ) {
+    try {
+      await this.taskService.createTask(authContext.userId, stackId, data);
+    } catch (error) {
+      throw error;
+    }
   }
 }
