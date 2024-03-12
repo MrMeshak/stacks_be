be4a0ebb4dcb347967d287e7be68f8d856472e2d
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2024-03-12 13:12:10 +1100

    change port to 10000

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials: true, origin: 'http://localhost:5173' });
+  app.enableCors({ credentials: true, origin: process.env.STACKS_FE_URL });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(4000);
+  await app.listen(10000);
 }
 bootstrap();
