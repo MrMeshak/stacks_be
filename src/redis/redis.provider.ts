eb1055c03f90017913480bf0cf65b38c4ed2a897
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2024-03-12 13:06:46 +1100

    adds default redis url

--- src/redis/redis.provider.ts
+++ src/redis/redis.provider.ts
@@ -4,7 +4,9 @@ import { Redis } from 'ioredis';
 export const redisProvider: FactoryProvider<Redis> = {
   provide: 'RedisClient',
   useFactory: async () => {
-    const redisClient = new Redis(process.env.REDIS_URL);
+    const redisClient = new Redis(
+      process.env.REDIS_URL || 'redis://red-cnn807f109ks73fveg6g:6379',
+    );
     return redisClient;
   },
 };
