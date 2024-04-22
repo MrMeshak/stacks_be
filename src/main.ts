c32370a10ab441fd0046c51b8c5e5db067a5ed05
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2024-04-22 12:10:34 +1000

    adds enviroment variable for port

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ credentials: true, origin: process.env.STACKS_FE_URL });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(10000);
+  await app.listen(process.env.PORT || 10000);
 }
 bootstrap();
